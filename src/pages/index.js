35d309448f5ab0390b78ee0c76a4aeea1765b37d
John Orlie Mascardo <johnorliem@gmail.com>
2026-02-08 21:44:58 +0800

    v2

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from './Home';
+import About from './About';
+import Techstack from './Techstack';
+import Projects from './Projects';
+import Certificates from './Certificates';
+import Contacts from './Contacts';
+
+export {
+    Home,
+    About,
+    Techstack,
+    Projects,
+    Certificates,
+    Contacts
+}
