27f94d08df3980488379526218709214a57353c4
John Orlie Mascardo <johnorliem@gmail.com>
2026-02-08 23:07:06 +0800

    Adding logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import portfolio_logo from "../assets/portfolio_logo.png";
 
 export default function Navbar() {
  return (
-  <nav>
-   <div className=" bg-[#4f5661] w-full">
+  <nav className="">
+   <div className="bg-[#4f5661] text-white">
     <div>
-     <ul className=' text-white'>
-       <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#home">Home</a></li>
-       <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#about">About</a></li>
-       <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#techstack">Tech Stack</a></li>
-       <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#project">Project</a></li>
-       <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#certificates">Certificates</a></li>
-       <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#contacts">Contacts</a></li>               
-      </ul>
+     <img src={portfolio_logo} alt="logo" className="w-auto h-auto" />
+      <ul className="flex justify-center items-center space-x-10 py-4">
+        <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#home">Home</a></li>
+        <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#about">About</a></li>
+        <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#techstack">Tech Stack</a></li>
+        <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#project">Project</a></li>
+        <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#certificates">Certificates</a></li>
+        <li className='bg-blue-950 px-4 py-1 rounded-4xl'><a href="#contacts">Contacts</a></li>               
+       </ul>
     </div>
-   </div>
-  </nav>
- );
+  </div>
+ </nav>
+);
 }
+
+
+/*
+   <nav className="w-full flex py-6 justify-between items-center navbar">
+   <img src={portfolio_logo} alt="logo" className="w-31 h-8" />
+  </nav>
+ */
