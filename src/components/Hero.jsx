d0cb0fcb32e4127baaf59fa683e55a09aae3fe16
John Orlie Mascardo <johnorliem@gmail.com>
2026-02-09 13:38:13 +0800

    Turning portfolio_logo to fixed and navigate to navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,12 @@ export default function Hero() {
     <section id="hero" className="h-screen flex items-center justify-center bg-deep-teal-200 pt-20">
       <div className="max-w-3xl text-center p-8 bg-deep-teal-600 text-white rounded-3xl shadow-lg">
         <img src={profile} alt="John Mascardo" className="w-36 h-36 rounded-full mx-auto mb-4" />
-        <h1 className="text-3xl md:text-4xl font-zalando-expanded-bold">Hi! I'm John Orlie T. Mascardo</h1>
-        <p className="mt-2 text-lg font-zalando-expanded-italic">An aspiring web developer.</p>
+        <h1 className="text-3xl md:text-4xl font-zalando-expanded-bold">
+          Hi! I'm John Orlie T. Mascardo
+        </h1>
+        <p className="mt-2 text-lg font-zalando-expanded-italic">
+          An aspiring web developer.
+        </p>
       </div>
     </section>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,13 @@ import portfolio_logo from "../assets/portfolio_logo.png";
 
 export default function Navbar() {
   return (
-    <div>
+    <div id ="navbar">
+      <a href='#navbar'>
+        <img src={portfolio_logo} alt="logo" className="fixed w-15 h-15" />
+      </a>
       <nav className="bg-deep-teal-400 font-zalando-italic">
         <div>
-          <img src={portfolio_logo} alt="logo" className="absolute w-15 h-15" />
           <ul className="flex justify-center items-center space-x-10 py-4 text-white">
-            <li className="px-4 py-1 rounded-4xl">
-              <a href="#home">Home</a>
-            </li>
             <li className="px-4 py-1 rounded-4xl">
               <a href="#about">About</a>
             </li>
