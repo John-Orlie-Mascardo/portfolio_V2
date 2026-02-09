42670e244e6f7f43e7a1f759047a1bedad57fc4a
John Orlie Mascardo <johnorliem@gmail.com>
2026-02-09 23:43:25 +0800

    Adding more contents to TechStack

--- src/assets/TechStack/index.js
+++ src/assets/TechStack/index.js
@@ -1,5 +1,28 @@
 import JavaLogo from "./Java-Logo.png";
-import CprogrammingLogo from "./c-programming.svg.png";
 import Dart from "./dart.png";
+import Html from "./html.svg.png";
+import Css from "./css.png";
+import Javascript from "./javascript.png";
+import Flutter from "./flutter.png";
+import ReactLogo from "./React-icon.svg.png";
+import Vscode from "./visual-studio.png";
+import Intellij from "./intellij.svg.png";
+import AndroidStudio from "./android-studio.svg.png";
+import Express from "./express.png";
+import Nodejs from "./node.png";
+import MongoDB from "./mongodb.png";
+import Git from "./git.png";
+import Github from "./github.png";
+import Render from "./render.png";
+import Vercel from "./vercel_logo.png";
+import Vite from "./Vitejs-logo.svg.png";
 
-export { JavaLogo, CprogrammingLogo, Dart };
+
+
+export { JavaLogo, Dart };
+
+export { Html, Css, Javascript, Flutter, ReactLogo };
+
+export { Vscode, Intellij, AndroidStudio };
+
+export { Express, Nodejs, MongoDB, Git, Github, Render, Vercel, Vite };
--- src/components/TechStackcard.jsx
+++ src/components/TechStackcard.jsx
@@ -2,7 +2,10 @@
 import React from "react";
 
 // Import images (Java, C programming, Dart) from the TechStack folder via index.js
-import { JavaLogo, CprogrammingLogo, Dart } from "../assets/TechStack/index.js";
+import { JavaLogo, Dart } from "../assets/TechStack/index.js";
+import {  Html, Css, Javascript, Flutter, ReactLogo } from "../assets/TechStack/index.js";
+import { Vscode, Intellij, AndroidStudio } from "../assets/TechStack/index.js";
+import { Express, Nodejs, MongoDB, Git, Github, Render, Vercel, Vite } from "../assets/TechStack/index.js";
 
 // Define and export the TechStackcard component
 export default function TechStackcard() {
@@ -26,42 +29,123 @@ export default function TechStackcard() {
           <div>
             {/* Subheading label */}
             <h3>Programming Language</h3>
+              <div className="flex justify-center">
+                 {/* Container to center the grid of logos */}
+                 
+                <div className="grid grid-cols-2 gap-4 max-w-2xl mx-auto">
+                  {/* Container for the Java logo.
+                  - grid + place-items-center centers content inside the box
+                  - w-32 h-32 gives the box a fixed size
+                  - mx-auto centers the box itself horizontally */}
 
-            <div>
-                {/* Container for the Java logo.
-                - grid + place-items-center centers content inside the box
-                - w-32 h-32 gives the box a fixed size
-                - mx-auto centers the box itself horizontally */}
+                  <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                    {/* Display the Java logo image
+                      - src={JavaLogo} uses the imported variable
+                      - alt provides descriptive text
+                      - w-20 h-20 sets the image size */}
+                    <img src={JavaLogo} alt="Java" className="w-20 h-20" />
+                  </div>
 
-              <div className="grid place-items-center w-32 h-32 mx-auto">
-                {/* Display the Java logo image
-                  - src={JavaLogo} uses the imported variable
-                  - alt provides descriptive text
-                  - w-20 h-20 sets the image size */}
-                <img src={JavaLogo} alt="Java" className="w-20 h-20" />
+                  <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                    {/* Container for the Dart Logo, same styling as Java */}
+                    <img src={Dart} alt="Dart" className="w-20 h-20" />
+                  </div>
+                </div>
               </div>
-
-              <div className="grid place-items-center w-32 h-32 mx-auto">
-                {/* Container for the C programming logo, same styling as Java */}
-                <img src={CprogrammingLogo} alt="C programming" className="w-20 h-20" />
-              </div>
-
-              <div className="grid place-items-center w-32 h-32 mx-auto">
-                {/* Container for the Dart Logo, same styling as Java */}
-                <img src={Dart} alt="Dart" className="w-20 h-20" />
-              </div>
-            </div>
-            
           </div>
-
-          {/* Placeholder headings for other categories */}
-          <h3>Frontend</h3>
-          <h3>IDE</h3>
-          <h3>Tools</h3>
-          
         </div>
 
-      </section>
-    </>
-  );
+        <div className="text-center text-white font-zalando-italic text-lg">
+          <div>
+            <h3>Frontend</h3>
+
+            <div className="flex justify-center">
+              <div className="grid grid-cols-2 gap-4 max-w-2xl mx-auto">
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Html} alt="Html" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Css} alt="Css" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Javascript} alt="Javascript" className="w-24 h-24" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Flutter} alt="Flutter" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center col-span-2 w-32 h-32 mx-auto">
+                  <img src={ReactLogo} alt="React" className="w-20 h-20" />
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>
+
+        <div className="text-center text-white font-zalando-italic text-lg">
+          <div>
+            <h3>IDE</h3>
+
+            <div className="flex justify-center">
+              <div className="grid grid-cols-2 gap-4 max-w-2xl mx-auto">
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Vscode} alt="Visual Studio Code" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Intellij} alt="IntelliJ" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center col-span-2 w-32 h-32 mx-auto">
+                  <img src={AndroidStudio} alt="Android Studio" className="w-20 h-20" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>  
+        
+        <div className="text-center text-white font-zalando-italic text-lg">
+          <h3>Tools</h3>
+        
+          <div className="flex justify-center">
+              <div className="grid grid-cols-2 gap-4 max-w-2xl mx-auto">
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Express} alt="Express" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Nodejs} alt="Nodejs" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={MongoDB} alt="MongoDB" className="w-24 h-24" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Git} alt="Git" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Github} alt="Github" className="w-20 h-20" />
+                </div>
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Render} alt="Render" className="w-24 h-24" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Vercel} alt="Vercel" className="w-20 h-20" />
+                </div>
+
+                <div className="flex items-center justify-center w-32 h-32 mx-auto">
+                  <img src={Vite} alt="Vite" className="w-20 h-20" />
+              </div>
+            </div>
+        </div>  
+      </div>
+    </section>
+  </>
+);
 }
